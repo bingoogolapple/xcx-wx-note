a17182626ce505311afcd2267726f904ef985655
bingoogolapple <bingoogolapple@gmail.com>
2020-01-25 20:59:39 +0800

    触发器

---
+++ cloudfunctions/trigger/index.js
@@ -0,0 +1,18 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  // https://developers.weixin.qq.com/miniprogram/dev/wxcloud/guide/functions/triggers.html
+  console.log('触发了触发器')
+  const wxContext = cloud.getWXContext()
+
+  return {
+    event,
+    openid: wxContext.OPENID,
+    appid: wxContext.APPID,
+    unionid: wxContext.UNIONID,
+  }
+}
