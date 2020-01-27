e4bb001fcae10d9941b6a2f853f42dc8b3706538
bingoogolapple <bingoogolapple@gmail.com>
2020-01-27 14:29:28 +0800

    扫码

--- miniprogram/pages/demo/cloud/cloud.js
+++ miniprogram/pages/demo/cloud/cloud.js
@@ -2,7 +2,8 @@
 
 Page({
   data: {
-    fileID: ''
+    fileID: '',
+    result: ''
   },
   sum: function() {
     wx.cloud.callFunction({
@@ -38,6 +39,27 @@ Page({
       })
     }).catch(err => {
       console.log('生成未限制小程序码失败', err)
+    })
+  },
+  scanCode: function () {
+    // https://developers.weixin.qq.com/miniprogram/dev/api/device/scan/wx.scanCode.html
+    wx.scanCode({
+      // onlyFromCamera: false,
+      // scanType: ['barCode', 'qrCode'],
+      success: res => {
+        // {charSet: "utf-8", result: "是的法师打发", errMsg: "scanCode:ok", rawData: "77u/5piv55qE5rOV5biI5omT5Y+R", scanType: "QR_CODE"}
+        console.log('扫码成功', res)
+        this.setData({
+          result: res.result
+        })
+      },
+      fail: err => {
+        if (err.errMsg.indexOf('cancel') != -1) {
+          console.log("取消扫码")
+        } else {
+          console.error('扫码失败', err)
+        }
+      }
     })
   },
   onShareAppMessage: function() {
