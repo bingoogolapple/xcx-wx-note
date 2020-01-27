9aeca3e29e57a60f4748cb893d8b1f641cb9242f
bingoogolapple <bingoogolapple@gmail.com>
2020-01-27 11:21:00 +0800

    微信登录

--- miniprogram/pages/movie/profile/profile.js
+++ miniprogram/pages/movie/profile/profile.js
@@ -1,73 +1,52 @@
-// miniprogram/pages/movie/profile/profile.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    userInfo: null
+  },
+  onShow: function() {
+    wx.getSetting({
+      success: settingRes => {
+        if (!settingRes.authSetting['scope.userInfo']) {
+          console.log('未授权，请登录', settingRes)
+          this.setData({
+            userInfo: null
+          })
+        } else if (!this.data.userInfo) {
+          console.log('已授权，未获取到用户数据，获取用户数据', settingRes)
+          wx.getUserInfo({
+            success: userInfoRes => {
+              console.log('获取用户信息成功', userInfoRes.userInfo)
+              this.setData({
+                userInfo: userInfoRes.userInfo
+              })
+            },
+            fail: err => {
+              console.error('获取用户信息失败', err)
+              this.setData({
+                userInfo: null
+              })
+            }
+          })
+        } else {
+          console.log('已授权，已获取到用户数据')
+        }
+      }
+    })
   },
   onGetUserInfo: function(event) {
-    // event.detail.userInfo
-    console.log(event.detail.userInfo)
+    console.log(event)
+    if (event.detail.userInfo) {
+      console.log('用户允许登录', event.detail.userInfo)
+      this.setData({
+        userInfo: event.detail.userInfo
+      })
+    } else {
+      console.error('用户拒绝登录', event)
+      this.setData({
+        userInfo: null
+      })
+    }
   },
   onGetPhoneNumber: function(event) {
     console.log(event)
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
