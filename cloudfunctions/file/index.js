2701258b9e6b2803c777b0f654895fd247ed393c
bingoogolapple <bingoogolapple@gmail.com>
2020-01-30 00:31:37 +0800

    用户登录授权流程

--- cloudfunctions/file/index.js
+++ cloudfunctions/file/index.js
@@ -6,19 +6,50 @@ cloud.init({
 })
 
 const db = cloud.database()
-const files = db.collection('files')
+const userInfos = db.collection('userInfos')
 
-async function test(ctx) {
+async function login(ctx) {
   try {
-    const event = ctx.event
-    
+    // 云端添加时默认不会添加 _openid 字段，需要单独设置
+    const user = ctx.event.user
+    let openid = ctx.event.userInfo.openId
+    user._openid = openid
+
+    let result = await userInfos.where({
+      _openid: openid
+    }).field({
+      _id: true
+    }).get()
+    // { data: [], errMsg: 'collection.get:ok' }
+    // { data: [ { _id: 'da51bd8c5e31a72b0794e37a2c7a1add' } ], errMsg: 'collection.get:ok' }
+    console.log('用户登录-查询结果', result)
+
+    if (result.data.length === 0) {
+      result = await userInfos.add({
+        data: user
+      })
+      // { _id: 'da51bd8c5e31a72b0794e37a2c7a1add', errMsg: 'collection.add:ok' }
+      console.log('用户登录-添加结果', result)
+      user._id = result._id
+    } else {
+      let id = result.data[0]._id
+      result = await userInfos.doc(id).set({
+        data: user
+      })
+      // 不管是 update 还是 set，都要放到调用后再设置 _id，否则会报错「不能更新_id的值;」
+      user._id = id
+      // update 方法（更新传入的字段）至少有一个字段变化时 updated 才为 1，否则为 0：{ stats: { updated: 0 }, errMsg: 'document.update:ok' }
+      // set 方法（替换除了 _id 以外的其他字段，查漏补缺）：{"_id":"da51bd8c5e31a72b0794e37a2c7a1add","errMsg":"document.set:ok","stats":{"updated":1,"created":0}}
+      // set 方法 不存在的 _id 进行 set 后 created 为 1：{"_id":"sdfsdfsdfsdfsdf","errMsg":"document.set:ok","stats":{"updated":0,"created":1}}
+      console.log('用户登录-更新结果', result)
+    }
 
     ctx.body = {
       code: 0,
-      data: result.data
+      data: user
     }
   } catch (err) {
-    console.error(err)
+    console.error('用户登录-失败', err)
     ctx.body = {
       code: 1,
       errMsg: err.errMsg
@@ -27,7 +58,7 @@ async function test(ctx) {
 }
 
 //---------------------------------------------------------------
-exports.main = async (event, context) => {
+exports.main = async(event, context) => {
   const log = cloud.logger()
   log.info(event)
 
@@ -35,13 +66,13 @@ exports.main = async (event, context) => {
     event
   })
 
-  app.use(async (ctx, next) => {
+  app.use(async(ctx, next) => {
     ctx.event = event
     ctx.context = context
     await next()
   })
 
-  app.router('test', test)
+  app.router('login', login)
 
   return app.serve()
 }
--- miniprogram/pages/demo/file/file.js
+++ miniprogram/pages/demo/file/file.js
@@ -1,7 +1,23 @@
-// miniprogram/pages/demo/file/file.js
+const db = wx.cloud.database()
+const userInfos = db.collection('userInfos')
+
 Page({
   data: {
-    avatorUrl: 'cloud://clinic-dev-gyarq.636c-clinic-dev-gyarq-1301144683/1579757364265.png'
+    avatorUrl: 'cloud://clinic-dev-gyarq.636c-clinic-dev-gyarq-1301144683/1579757364265.png',
+    userInfos: null
+  },
+  onLoad: function(options) {
+    this.loadUserInfos()
+  },
+  loadUserInfos: function() {
+    userInfos.get().then(res => {
+      console.log('获取用户列表成功', res)
+      this.setData({
+        userInfos: res.data
+      })
+    }).catch(err => {
+      console.error('获取用户列表失败', err)
+    })
   },
   upload: function() {
     wx.chooseImage({
@@ -75,18 +91,53 @@ Page({
   },
   deleteFile: function() {
     wx.cloud.deleteFile({
-      fileList: ['cloud://clinic-dev-gyarq.636c-clinic-dev-gyarq-1301144683/1579757975128.jpg']
-    })
-    .then(console.log)
-    .catch(console.error)
+        fileList: ['cloud://clinic-dev-gyarq.636c-clinic-dev-gyarq-1301144683/1579757975128.jpg']
+      })
+      .then(console.log)
+      .catch(console.error)
   },
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
     return {
       title: '测试标题',
       path: 'pages/demo/database/info/info',
       query: 'id=74b140b45e2d25aa05ea5d1b385fd88f',
       // imageUrl: 'cloud://clinic-dev-gyarq.636c-clinic-dev-gyarq-1301144683/1579757364265.png' // 图片地址 或 fileID 都可以
       imageUrl: 'http://bgashare.bingoogolapple.cn/adapter/imgs/8.png' // 图片地址 或 fileID 都可以
+    }
+  },
+  openUploadPage: function() {
+    console.log('openUploadPage')
+  },
+  onGetUserInfo: function(event) {
+    if (event.detail.userInfo) {
+      console.log('用户允许登录', event.detail.userInfo)
+
+      wx.showLoading({
+        title: '登录中...',
+        mask: true
+      })
+      wx.cloud.callFunction({
+        name: 'file',
+        data: {
+          $url: 'login',
+          user: event.detail.userInfo
+        }
+      }).then(res => {
+        wx.hideLoading()
+        if (res.result.code == 0) {
+          console.log('登录成功', res)
+          wx.navigateTo({
+            url: './add/add'
+          })
+        } else {
+          console.error('登录失败', res.result.errMsg)
+        }
+      }).catch(err => {
+        wx.hideLoading()
+        console.error('异常：登录失败', err)
+      })
+    } else {
+      console.error('用户拒绝登录', event)
     }
   }
 })
