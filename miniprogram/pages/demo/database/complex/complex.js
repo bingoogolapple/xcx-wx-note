72cf0e36cd6f7fb221ca8245b8baf6bd496ab891
bingoogolapple <bingoogolapple@gmail.com>
2020-01-29 10:54:33 +0800

    模糊搜索

--- miniprogram/pages/demo/database/complex/complex.js
+++ miniprogram/pages/demo/database/complex/complex.js
@@ -10,7 +10,50 @@ Page({
   },
   onLoad: function(options) {
     // this.orderByQuery()
-    this.batchRefresh()
+    // this.batchRefresh()
+    this.regExpQuery()
+  },
+  regExpQuery: function() {
+    products.where({
+      address: _.exists(true),
+      source: _.exists(true),
+      // source: /niPro/ // miniProgram 默认情况下大小写敏感
+      // source: /niPro/i // miniProgram、miniprogram 加上 i 表示大小写不敏感
+      // address: /aa/ // aa@gmail.com、aabb@oo.xx、bbaa@xx.oo
+      // address: /^aa/ // aa@gmail.com、aabb@oo.xx 开始匹配符 ^，加上 m 标记后还匹配行的开头
+      // address: /oo/ // aabb@oo.xx、bbaa@xx.oo
+      // address: /oo$/ // bbaa@xx.oo 结束匹配符 $，加上 m 标记后还匹配行的结尾
+      // address: /a[1-3]{3}a/ // a123a@gmail.com
+      // address: /a[1-3]+a/ // a123a@gmail.com
+      // address: /a\d{3}a/ // a123a@gmail.com
+      // address: /a\d*a/ // a123a@gmail.com、aa@gmail.com  *表示可有可无
+      // address: /a\da/ // 写了 \d、[1-3] 等模糊匹配后如果不写个数限定的话表示必须出现一次
+      // address: /a\d\d\da/ // a123a@gmail.com
+      // address: /a\d+a/ // a123a@gmail.com  +表示至少有一个
+      // address: /a.+@/ // a123a@gmail.com、aa@gmail.com  .表示除了换行符外的所有字符，如果加了s则标识所有字符
+
+      // source: db.RegExp({ regexp: 'niPro' }) // miniProgram 默认情况下大小写敏感
+      // source: db.RegExp({ regexp: 'niPro', options: 'i' }) // miniProgram、miniprogram 加上 i 表示大小写不敏感
+      // address: db.RegExp({ regexp: 'aa' }) // aa@gmail.com、aabb@oo.xx、bbaa@xx.oo
+      // address: db.RegExp({ regexp: '^aa' }) // aa@gmail.com、aabb@oo.xx 开始匹配符 ^，加上 m 标记后还匹配行的开头
+      // address: db.RegExp({ regexp: 'oo' }) // aabb@oo.xx、bbaa@xx.oo
+      // address: db.RegExp({ regexp: 'oo$' }) // bbaa@xx.oo 结束匹配符 $，加上 m 标记后还匹配行的结尾
+      // address: db.RegExp({ regexp: 'a[1-3]{3}a' }) // a123a@gmail.com
+      // address: db.RegExp({ regexp: 'a[1-3]+a' }) // a123a@gmail.com
+      // address: db.RegExp({ regexp: 'a\\d{3}a' }) // a123a@gmail.com 这种方式需要多加个斜杠来转义
+      // address: db.RegExp({ regexp: 'a\\d*a' }) // a123a@gmail.com、aa@gmail.com  *表示可有可无
+      // address: db.RegExp({ regexp: 'a\\da' }) // 写了 \d、[1-3] 等模糊匹配后如果不写个数限定的话表示必须出现一次
+      // address: db.RegExp({ regexp: 'a\\d\\d\\da' }) // a123a@gmail.com
+      // address: db.RegExp({ regexp: 'a\\d+a' }) // a123a@gmail.com  +表示至少有一个
+      // address: db.RegExp({ regexp: 'a.+a' }) // a123a@gmail.com、aa@gmail.com  .表示除了换行符外的所有字符，如果加了s则标识所有字符
+    }).get().then(res => {
+      console.log('小程序端加载产品列表成功', res.data)
+      this.setData({
+        products: res.data
+      })
+    }).catch(err => {
+      console.error('小程序端加载产品列表失败', err)
+    })
   },
   orderByQuery: function() {
     products.where({
