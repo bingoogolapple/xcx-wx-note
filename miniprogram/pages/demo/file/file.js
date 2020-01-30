e9a13d0f56e12b22bb8414ce40c1418c1b6f014f
bingoogolapple <bingoogolapple@gmail.com>
2020-01-30 23:45:45 +0800

    修改demo

--- miniprogram/pages/demo/file/file.js
+++ miniprogram/pages/demo/file/file.js
@@ -3,7 +3,6 @@ const userInfos = db.collection('userInfos')
 
 Page({
   data: {
-    avatorUrl: 'cloud://clinic-dev-gyarq.636c-clinic-dev-gyarq-1301144683/1579757364265.png',
     userInfos: null
   },
   onLoad: function(options) {
@@ -18,95 +17,6 @@ Page({
     }).catch(err => {
       console.error('获取用户列表失败', err)
     })
-  },
-  upload: function() {
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['original', 'compressed'],
-      sourceType: ['album', 'camera'],
-      success: res => {
-        // tempFilePath 可以作为 img 标签的 src 属性显示图片
-        const tempFilePaths = res.tempFilePaths
-        console.log(tempFilePaths)
-        const avatorUrl = tempFilePaths[0]
-        this.setData({
-          avatorUrl: avatorUrl
-        })
-        this.performUpload(avatorUrl)
-      },
-      fail: err => {
-        console.log(err)
-      }
-    })
-  },
-  performUpload: function(avatorUrl) {
-    wx.cloud.uploadFile({
-      cloudPath: 'demo/avator1.png',
-      filePath: avatorUrl,
-      formData: {
-        'key1': 'value1'
-      },
-      success: res => {
-        console.log(res)
-      },
-      fail: err => {
-        console.log(err)
-      }
-    })
-  },
-  download: function() {
-    wx.cloud.downloadFile({
-      fileID: 'cloud://clinic-dev-gyarq.636c-clinic-dev-gyarq-1301144683/demo/avator.png'
-    }).then(res => {
-      console.log(res)
-      this.setData({
-        avatorUrl: res.tempFilePath
-      })
-
-      wx.saveImageToPhotosAlbum({
-        filePath: res.tempFilePath,
-        success(res) {
-          console.log('保存成功', res)
-          wx.showToast({
-            title: '保存成功'
-          })
-        },
-        fail(err) {
-          console.log('保存失败', err)
-        }
-      })
-    }).catch(err => {
-      console.log(err)
-    })
-  },
-  getTempFileUrl: function() {
-    wx.cloud.getTempFileURL({
-        fileList: [
-          'cloud://clinic-dev-gyarq.636c-clinic-dev-gyarq-1301144683/demo/avator.png',
-          'cloud://clinic-dev-gyarq.636c-clinic-dev-gyarq-1301144683/demo/avator1.png'
-        ]
-      })
-      .then(res => console.log(res))
-      .catch(err => console.error(res))
-  },
-  deleteFile: function() {
-    wx.cloud.deleteFile({
-        fileList: ['cloud://clinic-dev-gyarq.636c-clinic-dev-gyarq-1301144683/1579757975128.jpg']
-      })
-      .then(console.log)
-      .catch(console.error)
-  },
-  onShareAppMessage: function() {
-    return {
-      title: '测试标题',
-      path: 'pages/demo/database/info/info',
-      query: 'id=74b140b45e2d25aa05ea5d1b385fd88f',
-      // imageUrl: 'cloud://clinic-dev-gyarq.636c-clinic-dev-gyarq-1301144683/1579757364265.png' // 图片地址 或 fileID 都可以
-      imageUrl: 'http://bgashare.bingoogolapple.cn/adapter/imgs/8.png' // 图片地址 或 fileID 都可以
-    }
-  },
-  openUploadPage: function() {
-    console.log('openUploadPage')
   },
   onGetUserInfo: function(event) {
     if (event.detail.userInfo) {
